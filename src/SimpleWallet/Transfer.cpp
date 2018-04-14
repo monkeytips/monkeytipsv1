429838cfa2944b6c6e7ccc0570a159f974ef800e
Vordas <suvakdavor@gmail.com>
2018-04-14 03:54:23 +0200

    Worktips adjustments

--- src/SimpleWallet/Transfer.cpp
+++ src/SimpleWallet/Transfer.cpp
@@ -26,7 +26,7 @@ bool parseAmount(std::string strAmount, uint64_t &amount)
 
     size_t pointIndex = strAmount.find_first_of('.');
     size_t fractionSize;
-    size_t numDecimalPlaces = 2;
+    size_t numDecimalPlaces = 8;
 
     if (std::string::npos != pointIndex)
     {
